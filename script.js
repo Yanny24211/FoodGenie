33c6cc3360cc46d09a5bf115e6a3a0e512195438
Bhavya Shah <woburnp@gmail.com>
2023-02-04 23:46:43 -0500

    API Fetch Complete with Dummy Values

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-async function getRecipe()
-{
-    const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=75e3c060122946e59f1a5c5c6782ca01&ingredients=${apple}&number=10&ignorePantry=true`)
-    const recipeSlip = await response.json();
-} 
+const API_KEY = "75e3c060122946e59f1a5c5c6782ca01";
+
+async function getRecipes(fruits) {
+  const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${fruits.join(',')}&apiKey=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+}
+
+const dummyFruits = ["apple", "banana"];
+
+getRecipes(dummyFruits).then(recipes => {
+  recipes.forEach(recipe => {
+    console.log(`ID: ${recipe.id}`);
+    console.log(`Title: ${recipe.title}`);
+    console.log(`Image: ${recipe.image}`);
+  });
+});
